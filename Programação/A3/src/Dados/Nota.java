d3fa99b7bad28c2bfa2ffa4511aba871bc47127a
Marcelo Pinotti <203372727+marcelopinotti@users.noreply.github.com>
2025-06-02 20:34:14 -0300

    Add files via upload

---
+++ Programação/A3/src/Dados/Nota.java
@@ -0,0 +1,37 @@
+package Dados;
+
+import java.util.Date;
+
+public class Nota {
+    private final int notaID;
+    private double valorNota;
+    private final Date dataLancamento;
+
+    public Nota(int notaID, double valorNota, Date dataLancamento) {
+     this.notaID = notaID;
+     this.valorNota = valorNota;
+     this.dataLancamento = dataLancamento;
+    }
+
+    public int getNotaID() {
+        return notaID;
+    }
+
+    public double getValorNota() {
+        return valorNota;
+    }
+
+    public void setValorNota(double valorNota) {
+        this.valorNota = valorNota;
+    }
+
+    public Date getDataLancamento() {
+        return dataLancamento;
+    }
+}
+
+
+
+
+
+
