4e2871969c054f120c3f443a9c3c4a3b60797240
Marcelo Pinotti <203372727+marcelopinotti@users.noreply.github.com>
2025-06-08 15:29:09 -0300

    Delete Programação directory

--- Programação/src/model/Administrador.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Administrador extends Funcionario {
-
-    public Administrador(int funcionarioID, String nome, String login, String senha, boolean ativo) {
-        super(funcionarioID, nome, login, senha, ativo);
-    }
-
-}
